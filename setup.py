9512be45a364ca9bda01110e2cf9b67614393e58
Zach Williams <hey@zachwill.com>
2012-01-19 23:49:31 -0800

    Add actual setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="dom",
+    version="0.1",
+    description="An easy-to-use command line utility for domain name lookups.",
+    author="Zach Williams",
+    author_email="hey@zachwill.com",
+    url="http://github.com/zachwill/dom",
+    license="MIT",
+    scripts=[
+        "dom"
+    ],
+    install_requires=[
+        "mock",
+        "requests",
+        "termcolor"
+    ]
+)
