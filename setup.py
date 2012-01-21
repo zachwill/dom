06b63bfb7deca94bde57b8b308848ee5cde22849
Zach Williams <hey@zachwill.com>
2012-01-20 17:37:38 -0800

    Update setup.py with classifiers

--- setup.py
+++ setup.py
@@ -10,6 +10,21 @@ setup(
     author_email="hey@zachwill.com",
     url="http://github.com/zachwill/dom",
     license="MIT",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
     packages=[
         "domainr"
     ],
