46f97d46d00db53652c15befc80613d899c313ff
Zach Williams <hey@zachwill.com>
2013-09-16 08:32:20 -0700

    Upgrade version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="dom",
-    version="0.4",
+    version="0.4.1",
     description="An easy-to-use command line utility for domain name lookups.",
     author="Zach Williams",
     author_email="hey@zachwill.com",
