32d85beb91cd4f4b9844bf0b12463fd5d4f8b656
Zach Williams <hey@zachwill.com>
2017-02-01 15:17:03 -0800

    Update version

--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@ from setuptools import setup
 
 setup(
     name="dom",
-    version="0.8",
+    version="0.9",
     description="An easy-to-use command line utility for domain name lookups.",
     author="Zach Williams",
     author_email="hey@zachwill.com",
     url="http://github.com/zachwill/dom",
     license="MIT",
     classifiers=[
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities'
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilitie",
     ],
     packages=[
         "domainr"
@@ -34,6 +34,6 @@ setup(
     install_requires=[
         "requests",
         "simplejson",
-        "termcolor"
+        "termcolor",
     ]
 )
